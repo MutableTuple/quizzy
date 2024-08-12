9ee5740ddc0ec01ba6d7bcdf705225aae738b714
MutableTuple <mutabletuple39@gmail.com>
2024-08-12 14:09:13 +0530

    fixed apos

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     serverActions: {
       // edit: updated to new key. Was previously `allowedForwardedHosts`
-      allowedOrigins: ["bn8hkfzn-3000.inc1.devtunnels.ms"],
+      allowedOrigins: ["https://quizzy-yoz2.vercel.app/"],
     },
   },
 };

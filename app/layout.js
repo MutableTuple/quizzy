76ab9ead60053cd97b7c14d5266699ad7f7b5bc7
MutableTuple <mutabletuple39@gmail.com>
2024-08-16 23:25:26 +0530

    added google gtag with next/thirdparty

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter, Poppins } from "next/font/google";
 import Head from "next/head";
 import "./globals.css";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600"],
@@ -16,24 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3X82FJRR76"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-3X82FJRR76');
-            `,
-          }}
-        />
-      </Head>
       <body className={poppins.className}>
         <main className="max-w-7xl mx-auto w-full mt-8">{children}</main>
+        <GoogleAnalytics gaId="G-QKDYP0VZC5" />
       </body>
     </html>
   );

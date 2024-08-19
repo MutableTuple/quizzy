42284ea13e1b22a4d36cab2acce538c97c79cf72
MutableTuple <mutabletuple39@gmail.com>
2024-08-20 00:15:54 +0530

    fixed answer bug

--- app/_components/SiginToSolve.jsx
+++ app/_components/SiginToSolve.jsx
@@ -14,7 +14,7 @@ export default async function SiginToSolve() {
     >
       <div className="flex sm:flex-row flex-col gap-2 text-sm sm:text-md items-center text-yellow-800">
         <IoIosWarning size={24} />
-        You must be logged in to solve today's question
+        You must be logged in to solve today&apos;s question
       </div>
       <button
         className="flex items-center gap-3 text-sm border border-blue-500 px-4 py-2 font-medium rounded-full  

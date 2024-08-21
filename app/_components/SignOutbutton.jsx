2b388a4074760d7cf297ea4aa244f11decb9e14f
MutableTuple <mutabletuple39@gmail.com>
2024-08-21 12:53:53 +0530

    fixed logout, form submit actions

--- app/_components/SignOutbutton.jsx
+++ app/_components/SignOutbutton.jsx
@@ -5,14 +5,11 @@ import { TbLogout } from "react-icons/tb";
 import { signOutAction } from "../_lib/actions";
 export default function SignOutbutton() {
   return (
-    <form
-      className="bg-red-200 p-1 rounded-full text-red-400 "
-      action={signOutAction}
-    >
-      <button>
+    <form className=" " action={signOutAction}>
+      <button className="flex items-center justify-center">
         <TbLogout
           size={16}
-          className="hover:cursor-pointer hover:text-red-700 duration-200 transition-all"
+          className="hover:cursor-pointer hover:text-red-900 duration-200 transition-all"
         />
       </button>
     </form>
